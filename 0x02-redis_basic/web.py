f21953d033eb1b97ced105d6f8662dbaf6bc24ba
samueltezera28 <samueltezera28@gmail.com>
2023-01-18 22:30:29 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,20 +2,41 @@
 """
 create a web cach
 """
-import redis
-import requests
-rc = redis.Redis()
-count = 0
+from typing import Optional
+from functools import wraps
+from cachetools import TTLCache
 
 
 def get_page(url: str) -> str:
     """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
+    if f"count:{url}" in cache:
+        cached_count = cache[f"count:{url}"]
+        cache[f"count:{url}"] = cached_count + 1
+    else:
+        cache[f"count:{url}"] = 1
+    
+    response = requests.get(url).text
+    return response
 
+
+
+def memozise(method):
+    @wraps(method)
+    def inner(url:str) -> str:
+        if f"count:{url}" in cache: 
+            cached_count = cache[f"count:{url}"]
+            cache[f"count:{url}"] = cached_count + 1
+        else:
+            cache[f"count:{url}"] = 1 
+            method_response = method(url)
+            cache[url] = method_response
+        return cache[url]
+    return inner
+
+@memozise
+def get_page(url: str) -> str:
+    return requests.get(url).text
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    get_page("http://slowwly.robertomurray.co.uk/delay/2000/url/https://www.python.org")
+    print(f"Total access of URL: {cache['count:http://slowwly.robertomurray.co.uk/delay/2000/url/https://www.python.org']}")
